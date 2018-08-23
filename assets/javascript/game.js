50af19c930e3dd2ba60e45575a5ab657959fdd5f
DrF <sp.knoll.86@gmail.com>
2018-08-22 21:21:21 -0500

    Word-Guess-Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var countryNames = ["united states", "canada", "mexico", "france", "spain", "germany", "brazil", "great britain", "china", "japan", "egypt", "australia", "india", "panama", "costa rica", "new zealand"];
+var wrongGuess = [];
+
+var wins = 0;
+var losses = 0;
+var remaining = 10;
+
